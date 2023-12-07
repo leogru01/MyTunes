2a958bbf26a59760fcada785a26ef62f47c8632b
leogru01 <143804597+leogru01@users.noreply.github.com>
2023-12-07 08:58:55 +0100

    updated

--- src/main/java/com/example/mytunes/HelloController.java
+++ src/main/java/com/example/mytunes/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.mytunes;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.util.List;
@@ -8,7 +9,10 @@ import java.util.List;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    @FXML
+    private Button NewOnPlaylist;
+    @FXML
+    private Button EditOnPlaylist, DeleteOnPlaylist, NewSongOnPlaylist, EditSongOnPlaylist, DeleteSongsOnPlaylist, CloseAppOnPlaylist;
     @FXML
     protected void onHelloButtonClick() {
 
